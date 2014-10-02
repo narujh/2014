3f12244fcc780e067f345bb57f71a8a0d42606d6
Pechenqko <creator_it@mail.ru>
2014-10-02 23:46:10 +0400

    Panasenko

--- ivb-3-14/Panasenko.A.V/Lab_01/lab_1/labor_1.cpp
+++ ivb-3-14/Panasenko.A.V/Lab_01/lab_1/labor_1.cpp
@@ -13,36 +13,34 @@ static double m2[] = { 1., 1. };
 
 static double m3[] = { 4., 4., 4., 4., 4., 4. };
 
-static double 
+static double
 calculate(double matrix[], int elements, const double part)
 {
 	double result = 0;
 	int i;
-	int n = 0;
-
-	for (i = 0; i < elements; ++i) {
+	for (i = 0; i < elements; i++) {
 		if (matrix[i] < part) {
 			result += matrix[i];
-			++n;
 		}
 	}
-	return result == 0 || n == 0 ? 0 : result / n;
+	return result;
 }
-
-int
-main(int argc, char **argv)
+ void min_mr(double mr1, double mr2, double mr3)
+{
+	 if (mr1 == mr2)
+		 fprintf(stdout, "Matrix1 == Matrix2\n");
+	 if (mr2 == mr3)
+		 fprintf(stdout, "Matrix2 == Matrix3\n");
+	 if (mr1 == mr3)
+		 fprintf(stdout, "Matrix1 == Matrix3\n");
+}
+int main(int argc, char **argv)
 {
 	const double part = 4.;
 	double mr1 = calculate(m1, MATRIX1, part);
 	double mr2 = calculate(m2, MATRIX2, part);
-	double mr3 = calculate(m3, sizeof(m3)/sizeof(m3[0]), part);
-
-	if (mr1 == mr2)
-		fprintf(stdout, "Matrix1 == Matrix2\n");
-	if (mr1 == mr3)
-		fprintf(stdout, "Matrix1 == Matrix3\n");
-	if (mr2 == mr3)
-		fprintf(stdout, "Matrix2 == Matrix3\n");
+	double mr3 = calculate(m3, MATRIX3, part);
+	min_mr(mr1, mr2, mr3);
 	system("pause");
 	return EXIT_SUCCESS;
 }
